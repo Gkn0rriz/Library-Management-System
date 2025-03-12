95388f1336d72714234c24deec25df9ada0619c3
Norris <gandanganorris@gmail.com>
2025-03-12 19:26:44 +0200

    added a service layer for the book entity

--- src/main/java/com/library_management_system/repository/BookRepository.java
+++ src/main/java/com/library_management_system/repository/BookRepository.java
@@ -1,7 +1,15 @@
 package com.library_management_system.repository;
 
 import com.library_management_system.entity.Book;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book,Long> {
+
+    List<Book> findByBookName(String title);
+
+    Page<Book> findByBookAuthor(String author);
+
 }
---
+++ src/main/java/com/library_management_system/service/BookService.java
@@ -0,0 +1,69 @@
+package com.library_management_system.service;
+
+import com.library_management_system.entity.Book;
+import com.library_management_system.repository.BookRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+    private final ModelMapper modelMapper;
+
+    public BookService(BookRepository bookRepository, ModelMapper modelMapper) {
+        this.bookRepository = bookRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public Page<Book> fetchAllBooks(Pageable pageable){
+        return bookRepository.findAll(pageable);
+    }
+
+    public Book fetchBookById(Long id){
+        return bookRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Book not found"));
+    }
+
+    public List<Book> fetchBookByTitle(String title ){
+        return bookRepository.findByBookName(title);
+    }
+
+    public Page<Book> fetchBookByAuthor(String author){
+        return bookRepository.findByBookAuthor(author);
+    }
+
+    public Boolean checkBookAvailability(Long id){
+        return bookRepository.existsById(id);
+    }
+
+    public Book addBook(Book book){
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Long id, Book book){
+        Book existingBook = bookRepository.findById(id).orElseThrow(()
+                -> new EntityNotFoundException("Book not found"));
+
+        modelMapper.map(book,existingBook);
+
+        return bookRepository.save(existingBook);
+
+    }
+
+    @Transactional
+    public void deleteBookById(Long id){
+
+        if (!bookRepository.existsById(id))
+            throw new EntityNotFoundException("Book not found");
+
+        bookRepository.deleteById(id);
+    }
+
+
+}
